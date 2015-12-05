aeab678f96a79c4370fdaac457c70db83a7edd0e
“Alvin <avinoz@gmail.com>
2015-12-04 16:57:57 -0800

    adds first set of slideshow

--- scripts/jquery.modal.js
+++ scripts/jquery.modal.js
@@ -4,21 +4,39 @@ $(document).ready(function(){
   $('.two').click(function(){
     $('.ftwo').slideToggle();
   });
-  $('.ftwo').click(function(){
-    $('.ftwo').slideToggle();
-  });
 
   $('.three').click(function(){
-    $('.fthree').slideToggle();
-  });
-  $('.fthree').click(function(){
     $('.fthree').slideToggle();
   });
 
   $('.four').click(function(){
     $('.ffour').slideToggle();
   });
-  $('.ffour').click(function(){
-    $('.ffour').slideToggle();
+
+  $('.close').click(function(){
+    $('.float').slideUp();
+  });
+
+
+  var pages = $('.slidecont li'), current=0;
+  var currentPage,nextPage;
+
+  $('.slidecont .button').click(function(){
+    currentPage= pages.eq(current);
+    if($(this).hasClass('prevButton')){
+      if (current <= 0)
+        current=pages.length-1;
+      else
+        current=current-1;
+    } else {
+      if (current >= pages.length-1)
+        current=0;
+      else
+        current=current+1;
+    }
+    nextPage = pages.eq(current);
+    currentPage.hide();
+    nextPage.show();
   });
 });
+

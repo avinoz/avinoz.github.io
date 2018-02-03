c10199eb84943795caf27f56128044acaf6a4a54
Alvin Shiu <avinoz@gmail.com>
2018-02-03 12:06:55 -0800

    fixed resize then move to bottom bug

--- arcadia challenge/js/resize.js
+++ arcadia challenge/js/resize.js
@@ -8,14 +8,17 @@ $(document).ready(function() {
 
       if (user == "left") {
         $(".menu-bar").removeClass("menu-bar-bottom");
+        $(".menu-bar").css("width", "200");
         $(".content-area").css("justify-content", "flex-start");
       } else if (user == "right") {
         $(".menu-bar").removeClass("menu-bar-bottom");
+        $(".menu-bar").css("width", "200");
         $(".content-area").css("justify-content", "flex-end");
       } else {
         $(".menu-bar").toggleClass("menu-bar-bottom");
-      }
+        $(".menu-bar").css("width", "100%");
 
+      }
   });
 
   $('.menu-bar').resizable({
